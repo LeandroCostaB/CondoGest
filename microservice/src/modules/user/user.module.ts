8a8bbe67ed466a5cab56d8b053a803d6e0b2942b
Luis <luisslindnerr@gmail.com>
2026-05-19 21:21:41 -0300

    feature/backend: integra criação de morador ao microserviço de envio de e-mail

--- microservice/src/modules/user/user.module.ts
+++ microservice/src/modules/user/user.module.ts
@@ -3,6 +3,7 @@ import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UserController } from './infra/controllers/users.controller';
 import { AuthService } from './application/services/auth.service';
+import { NotificationDispatchService } from './application/services/notification-dispatch.service';
 import { UserService } from './application/services/user.service';
 import { NotificationPayloadService } from './application/services/notification-payload.service';
 
@@ -19,7 +20,12 @@ import { NotificationPayloadService } from './application/services/notification-
     }),
   ],
   controllers: [UserController],
-  providers: [AuthService, UserService, NotificationPayloadService],
+  providers: [
+    AuthService,
+    UserService,
+    NotificationDispatchService,
+    NotificationPayloadService,
+  ],
   exports: [AuthService],
 })
 export class UserModule {}
