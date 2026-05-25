8ae03195466c74d3f63ca002771585debe76f9bf
WWexe <oeningvinicius@gmail.com>
2026-05-25 20:13:42 -0300

    implementa ajuste

--- mobile_condogest/lib/main.dart
+++ mobile_condogest/lib/main.dart
@@ -1,6 +1,4 @@
-import 'package:condogest/features/property_maintenance/data/datasources/i_maintenance_service.dart';
-import 'package:condogest/features/property_maintenance/data/datasources/maintenance_service.dart';
-import 'package:condogest/features/property_maintenance/presentation/viewmodels/maintenance_viewmodel.dart';
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter/foundation.dart';
@@ -11,9 +9,13 @@ import 'core/database/database_helper.dart';
 import 'features/property_manager/data/datasources/i_property_service.dart';
 import 'features/property_manager/data/datasources/property_service.dart';
 import 'features/property_manager/presentation/viewmodels/property_viewmodel.dart';
+import 'features/property_manager/domain/repositories/property_repository.dart';
+import 'features/property_manager/data/repositories/property_repository_impl.dart';
 import 'features/auth/data/datasources/auth_service.dart';
 import 'features/auth/data/datasources/i_auth_service.dart';
 import 'features/auth/presentation/viewmodels/auth_view_model.dart';
+import 'features/auth/presentation/pages/auth_view.dart';
+import 'features/auth/domain/entities/user_entity.dart';
 import 'features/ticket_manager/domain/repositories/ticket_repository.dart';
 import 'features/ticket_manager/data/repositories/ticket_repository_impl.dart';
 import 'features/ticket_manager/data/datasources/ticket_local_datasource.dart';
@@ -22,16 +24,14 @@ import 'core/presentation/pages/main_navigation_screen.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Desktop support for sqflite
   if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
     sqfliteFfiInit();
     databaseFactory = databaseFactoryFfi;
   }
-  
+
   final dbHelper = DatabaseHelper();
   final db = await dbHelper.database;
-  
-  // Repositories Initialization
+
   final ticketRepo = TicketRepositoryImpl(TicketLocalDatasource(db));
   final propertyService = PropertyService(db);
   final propertyRepo = PropertyRepositoryImpl(propertyService);
@@ -39,15 +39,13 @@ void main() async {
   runApp(
     MultiProvider(
       providers: [
-        Provider<IAuthService>(create: (_) => AuthService()),
+        Provider<IAuthService>(create: (_) => AuthService(db)),
         ChangeNotifierProvider(
           create: (context) => AuthViewModel(context.read<IAuthService>()),
         ),
-
         Provider<IPropertyService>(create: (_) => propertyService),
         Provider<PropertyRepository>(create: (_) => propertyRepo),
         Provider<TicketRepository>(create: (_) => ticketRepo),
-        
         ChangeNotifierProvider(
           create: (context) =>
               PropertyViewModel(context.read<IPropertyService>()),
@@ -63,31 +61,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-<<<<<<< HEAD
-    return MultiProvider(
-      providers: [
-        Provider<IAuthService>(create: (_) => AuthService()),
-        ChangeNotifierProvider(
-          create: (context) => AuthViewModel(context.read<IAuthService>()),
-        ),
-
-        Provider<IPropertyService>(create: (_) => PropertyService()),
-        ChangeNotifierProvider(
-          create: (context) =>
-              PropertyViewModel(context.read<IPropertyService>()),
-        ),
-        Provider<IMaintenanceService>(create: (_) => MaintenanceService()),
-        ChangeNotifierProvider(
-          create: (context) =>
-              MaintenanceViewModel(context.read<IMaintenanceService>()),
-        ),
-      ],
-      child: MaterialApp(
-        title: 'CondoGest',
-        debugShowCheckedModeBanner: false,
-        //home: const LoginView(),
-        home: const AppRoot(),
-=======
     return MaterialApp(
       title: 'CondoGest',
       debugShowCheckedModeBanner: false,
@@ -98,16 +71,24 @@ class MyApp extends StatelessWidget {
       home: Consumer<AuthViewModel>(
         builder: (context, auth, _) {
           if (auth.isLoading) {
-            return const Scaffold(body: Center(child: CircularProgressIndicator()));
-          }
-          if (auth.isAuthenticated) {
-            return MainNavigationScreen(
-              ticketRepository: context.read<TicketRepository>(),
+            return const Scaffold(
+              body: Center(child: CircularProgressIndicator()),
             );
           }
-          return const AppRoot(); // Handles login view
+          if (auth.isAuthenticated) {
+            final user = auth.currentUser;
+            if (user != null) {
+              final userType = (user.type == UserRole.resident)
+                  ? 'resident'
+                  : 'syndic';
+              return MainNavigationScreen(
+                ticketRepository: context.read<TicketRepository>(),
+                userType: userType,
+              );
+            }
+          }
+          return const LoginView();
         },
->>>>>>> mobile_/feature/SCRUM-23
       ),
     );
   }
